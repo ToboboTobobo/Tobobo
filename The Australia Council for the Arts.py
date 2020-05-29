0640e4802e910df9fe7b9812afa12ef59645c528
k07224115 <61529435+k07224115@users.noreply.github.com>
2020-05-30 02:38:38 +0800

    Create The Australia Council for the Arts.py

---
+++ The Australia Council for the Arts.py
@@ -0,0 +1,23 @@
+import calendar
+translator_a = googletrans.Translator()
+#translator_b = translate.Translator(to_lang = "zh-tw")
+url = "https://www.australiacouncil.gov.au/news/media-centre/media-releases/"
+title, link, translate, date = [], [], [], []
+response = requests.get(url)
+response.encoding = "utf-8"
+soup = BeautifulSoup(response.text, "lxml")
+now = datetime.datetime.now()
+yesterday = datetime.datetime.now() + datetime.timedelta(-1)
+for i in soup.find_all("p", class_ = "date"):
+    month = calendar.month_name[yesterday.month]
+    if f"{yesterday.day} {month} {yesterday.year}" in i.text:
+        date.append(f"{yesterday.year}/{yesterday.month}/{yesterday.day}")
+        j = i.find_previous("h3")
+        title.append(j.a.text)
+        link.append(j.a["href"])
+        try:
+            translate.append(translator_a.translate(j.a.text, dest = "zh-tw").text)
+        except:
+            translate.append(translator_b.translate(title_ + "/" + subtitle))
+result = pd.DataFrame({"編號":"", "撰寫":"", "搜尋日期":f"{now.year}/{now.month}/{now.day}", "新聞日期":date, "地區別":"", "國家":"", "主題類別":"", "發佈機構/來源":"The Australia Council for the Arts", "標題翻譯":translate, "標題原文":title, "關鍵字":"", "負責人":"", "連結":link, "備註":""})
+result
